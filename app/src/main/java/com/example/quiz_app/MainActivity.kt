1b78345b464913fe4e7df376760aa887479cb7f6
Aditya Desai <115383821+aditya-desai-19@users.noreply.github.com>
2023-05-21 13:49:06 +0530

    Added button to handle fragments.

--- app/src/main/java/com/example/quiz_app/MainActivity.kt
+++ app/src/main/java/com/example/quiz_app/MainActivity.kt
@@ -2,10 +2,9 @@ package com.example.quiz_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
