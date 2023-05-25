e4b3e31b171b7d7b12b7f2537f3bb1ce6ffa0312
Aditya Desai <officialdesaiaditya@gmail.com>
2023-05-25 13:25:31 +0530

    Revert "Commit changes"
    This reverts commit 2c57b586e2d9cd8d2589ea566e6e312ba2bc74a3.

--- app/src/main/java/com/example/quiz_app/MainActivity.kt
+++ app/src/main/java/com/example/quiz_app/MainActivity.kt
@@ -4,17 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import androidx.fragment.app.FragmentContainerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fragment = StartFragment()
-        val container = findViewById<FragmentContainerView>(R.id.container)
-        supportFragmentManager.beginTransaction()
-            .add(R.id.container, fragment)
-            .commit()
+        val fragment = ButtonFragment()
+        val tv = fragment.view?.findViewById<Button>(R.id.btn)
+        tv?.text = "Enter"
+        val fragmentManager = supportFragmentManager
+        val trans = fragmentManager.beginTransaction()
+        trans.add(R.id.container, fragment)
+        trans.commit()
     }
 }
